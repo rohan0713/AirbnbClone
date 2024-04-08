c27ff5a9661269b696a3dca84294aaf5913a3f42
RohanRamola <rohanramola63@gmail.com>
2024-04-08 10:30:45 +0530

    Modified Profile Screen

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -6,56 +6,275 @@ import {
   SafeAreaView,
   StyleSheet,
   Image,
+  ScrollView,
 } from "react-native";
-
-import image1 from "../assets/static/ic_views.png";
-import image2 from "../assets/static/ic_pool.png";
-import image3 from "../assets/static/ic_omg.png";
-import image4 from "../assets/static/ic_ski.png";
-import image5 from "../assets/static/ic_lake.png";
-import image6 from "../assets/static/ic_luxe.png";
-
-const images = [image1, image2, image3, image4, image5, image6];
-
-const items = [
-  { id: 1, name: "Amazing view", icon: 0 },
-  { id: 2, name: "Amazing pools", icon: 1 },
-  { id: 3, name: "OMG!", icon: 2 },
-  { id: 4, name: "Skiing", icon: 3 },
-  { id: 5, name: "Lakes", icon: 4 },
-  { id: 6, name: "Luxe", icon: 5 },
-];
-
-const renderItem = ({ item }) => (
-  <View style={{ padding: 5, backgroundColor: "white", alignItems: "center" }}>
-    <Image source={images[item.icon]} />
-    <Text style={{ padding: 5 }}>{item.name}</Text>
-  </View>
-);
+import { colors } from "../assets/colors/color";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 function ProfileScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        style={styles.list}
-        data={items}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        horizontal
-      />
-    </SafeAreaView>
+    <ScrollView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Profile</Text>
+          <MaterialCommunityIcons
+            name="bell-ring-outline"
+            size={30}
+            color="black"
+            style={styles.bell}
+          />
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            margin: 20,
+          }}
+        >
+          <Image
+            style={{
+              width: 80,
+              height: 80,
+              borderRadius: 40,
+            }}
+            source={require("../assets/static/icon.png")}
+          />
+          <View
+            style={{
+              marginHorizontal: 15,
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ fontSize: 16, fontWeight: "400" }}>Harry Smith</Text>
+            <Text style={{ fontWeight: 300, fontSize: 12 }}>Show Profile</Text>
+          </View>
+        </View>
+
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            marginVertical: 30,
+            marginHorizontal: 20,
+          }}
+        >
+          <Text style={styles.title}>Settings</Text>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <Feather name="info" size={20} color="black" />
+          <Text>Personal Information</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <MaterialIcons name="security" size={20} color="black" />
+          <Text>Login & security</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <MaterialIcons name="payments" size={20} color="black" />
+          <Text>Payments and payouts</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <MaterialIcons name="app-settings-alt" size={20} color="black" />
+          <Text>Accessibility</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <MaterialIcons name="folder-copy" size={20} color="black" />
+          <Text>Taxes</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <MaterialIcons name="translate" size={20} color="black" />
+          <Text>Translation</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <MaterialIcons name="notifications" size={20} color="black" />
+          <Text>Notifications</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <MaterialIcons
+            name="security-update-warning"
+            size={20}
+            color="black"
+          />
+          <Text>Privacy and sharing</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <MaterialIcons name="wallet-travel" size={20} color="black" />
+          <Text>Travel for work</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            marginVertical: 30,
+            marginHorizontal: 20,
+          }}
+        >
+          <Text style={styles.title}>Support</Text>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <MaterialIcons name="question-answer" size={20} color="black" />
+          <Text>Visit the Help Center</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <MaterialIcons name="medical-services" size={20} color="black" />
+          <Text>Get help with a safety issue</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <FontAwesome5 name="airbnb" size={20} color="black" />
+          <Text>How Airbnb works</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            marginHorizontal: 20,
+            gap: 20,
+          }}
+        >
+          <MaterialIcons name="edit" size={20} color="black" />
+          <Text>Give us feedback</Text>
+        </View>
+        <View style={styles.divider}></View>
+
+        <Text
+          style={{
+            fontSize: 16,
+            margin: 20,
+            color: "red",
+            fontWeight: "bold",
+            textDecorationLine: "underline",
+          }}
+        >
+          Log out
+        </Text>
+      </SafeAreaView>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "orange",
+    backgroundColor: colors.background,
   },
 
-  list: {
-    top: 50,
-    backgroundColor: "yellow",
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 80,
+    marginHorizontal: 20,
+  },
+
+  title: {
+    fontSize: 28,
+    fontWeight: "500",
+  },
+
+  bell: {
+    marginTop: 5,
+  },
+
+  divider: {
+    margin: 20,
+    height: 0.8,
+    backgroundColor: "lightgrey",
   },
 });
 
